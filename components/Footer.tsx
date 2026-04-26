2addf3b32a894126c0fc03fb305ebaaf68433cc4
Johnspiritual <111204126+Johnspiritual@users.noreply.github.com>
2026-04-26 17:11:43 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -97,7 +97,7 @@ const Footer = () => {
               </li>
               <li className="pt-4">
                 <a
-                  href="[wa.me](https://wa.me/2348101229792)"
+                  href="https://wa.me/2348101229792"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="inline-flex items-center px-4 py-2 bg-green-500 hover:bg-green-600 rounded-lg transition-colors text-sm font-medium"
